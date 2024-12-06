aa06874497c4e9e855554dbea6efded13f4680f4
thaminultazbir <thaminultazbir1999@gmail.com>
2024-12-06 12:23:21 +0600

    logic implementing

--- main.js
+++ main.js
@@ -1,5 +1,45 @@
 'use strict';
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
+
 document.querySelector('.check').addEventListener('click', function(){
-    console.log(document.querySelector('.guess').value);
+    const guess = Number(document.querySelector('.guess').value);
+
+    if(!guess){
+        document.querySelector('.message').textContent = '⛔ No Number';
+    }
+
+
+    else if(guess === secretNumber){
+        document.querySelector('.message').textContent = '🥇 Correct Number';
+    }
+
+
+    else if(guess > secretNumber){
+        if(score > 1){
+            document.querySelector('.message').textContent = '📈 Too High';
+            score--;
+            document.querySelector('.score').textContent = score;
+        }
+        else{
+            document.querySelector('.message').textContent = 'You lost the game 😥';
+            document.querySelector('.score').textContent = 0;
+        }
+    }
+
+    else if(guess < secretNumber){
+
+        if(score > 1){
+            document.querySelector('.message').textContent = '📉 Too Low';
+            score--;
+            document.querySelector('.score').textContent = score;
+        }
+        else{
+            document.querySelector('.message').textContent = 'You lost the game 😥';
+            document.querySelector('.score').textContent = 0;
+        }
+    }
 })
