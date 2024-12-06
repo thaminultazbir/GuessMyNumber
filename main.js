db4f84847ae124171329c2f3bd839fd2292676e0
thaminultazbir <thaminultazbir1999@gmail.com>
2024-12-06 18:24:03 +0600

    Project Done

--- main.js
+++ main.js
@@ -1,22 +1,34 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
+let highScore = 0;
 
 
 document.querySelector('.check').addEventListener('click', function(){
     const guess = Number(document.querySelector('.guess').value);
 
+    // When No Number
+
     if(!guess){
         document.querySelector('.message').textContent = '⛔ No Number';
     }
 
+    // When Player Wins
 
     else if(guess === secretNumber){
         document.querySelector('.message').textContent = '🥇 Correct Number';
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+        document.querySelector('.number').textContent = secretNumber;
+
+        if(score > highScore){
+            highScore = score;
+            document.querySelector('.highscore').textContent = highScore;
+        }
     }
 
+    // When Guess High
 
     else if(guess > secretNumber){
         if(score > 1){
@@ -30,6 +42,7 @@ document.querySelector('.check').addEventListener('click', function(){
         }
     }
 
+    // When Guess Low
     else if(guess < secretNumber){
 
         if(score > 1){
@@ -42,4 +55,15 @@ document.querySelector('.check').addEventListener('click', function(){
             document.querySelector('.score').textContent = 0;
         }
     }
-})
+});
+
+document.querySelector('.again').addEventListener('click', function(){
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+});
